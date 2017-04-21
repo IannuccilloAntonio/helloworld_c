3c3bc9888ae20d9d08178f4458404b112fba59ce
Antonio Iannuccillo <iannuccillo.antonio99@gmail.com>
2017-04-21 08:27:57 +0200

    Update maggiore_tra_due.c

--- maggiore_tra_due.c
+++ maggiore_tra_due.c
@@ -3,17 +3,16 @@
 int main() {
     int n1;
     int n2;
-
+    int max;
     printf("Inserisci il primo numero ");
     scanf("%d", &n1);
 
     printf("Inserisci il secondo numero ");
     scanf("%d", &n2);
-    
-    if (n1>=n2){
-      printf("Il maggiore e' a: %d\n", n1);
-    }
-    printf("Il maggiore e' b: %d\n", n2);
+     max=n1;
+    if (n2>max){
+      max=n2;}
+	printf("%d", max);
 	
 return 0;
 	}
