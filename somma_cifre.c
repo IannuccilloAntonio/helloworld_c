a5bc488560c6c664de21a198fddc4fe9c6bb67cf
IannuccilloAntonio <iannuccillo.antonio99@gmail.com>
2017-04-24 19:09:13 +0200

    fatto!

--- somma_cifre.c
+++ somma_cifre.c
@@ -2,7 +2,7 @@
 
 int main(){
 
-int num;
+int num, somma;
 int cifra1, cifra2, cifra3;
 
 printf("Inserisci un numero di tre cifre");
@@ -11,7 +11,14 @@ scanf("%d", &num);
 if (num<0 ||num>999){
 	printf("devi inserire TRE CIFRE");}
 else {
-	cifra3 = num/10;}
-	printf("%d", cifra3 + num);
+	cifra3 = num%10;
+	num = num/10;
+	cifra2 = num%10;
+	num = num/10;
+	cifra1 = num%10;
+	num = num/10;
+}
+somma = cifra3 + cifra2 + cifra1;
+	printf("La somma tra %d + %d + %d e'%d", cifra1, cifra2, cifra3, somma );
 return 0; }
 	
