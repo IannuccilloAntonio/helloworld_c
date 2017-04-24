936388fccc7f854a151a3aa3cef1edc99079722b
IannuccilloAntonio <iannuccillo.antonio99@gmail.com>
2017-04-24 16:08:00 +0200

    mi escono solo le prime due cifre

---
+++ somma_cifre.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(){
+
+int num;
+int cifra1, cifra2, cifra3;
+
+printf("Inserisci un numero di tre cifre");
+scanf("%d", &num);
+
+if (num<0 ||num>999){
+	printf("devi inserire TRE CIFRE");}
+else {
+	cifra3 = num/10;}
+	printf("%d", cifra3 + num);
+return 0; }
+	
